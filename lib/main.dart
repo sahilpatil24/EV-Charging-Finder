7ff2d2b376c646add4620edd493c9c897ad0e5bb
patilsahil2404-code <patilsahil2404@gmail.com>
2026-02-19 09:22:24 +0530

    initiated openchargemap api - sahil (as of 19/02/2026)
    Added red markers to stations
    added open charge map api

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 
 void main() {
   runApp(const MyApp());
@@ -33,8 +36,10 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  static const String apiKey = "b158c085-25b6-423d-b166-afb61865b11a";
   GoogleMapController? _mapController;
   LatLng? _currentPosition;
+  Set<Marker> _markers = {};
   int _selectedIndex = 0;
 
   final Color primaryGreen = const Color(0xFF12B886);
@@ -44,6 +49,136 @@ class _HomeScreenState extends State<HomeScreen> {
     super.initState();
     _getCurrentLocation();
   }
+  Future<void> fetchStations(double lat, double lon) async {
+    final url = Uri.parse(
+        "https://api.openchargemap.io/v3/poi/"
+            "?output=json"
+            "&latitude=$lat"
+            "&longitude=$lon"
+            "&distance=15"
+            "&distanceunit=KM"
+            "&maxresults=20"
+    );
+
+    try {
+      final response = await http.get(
+        url,
+        headers: {
+          "X-API-Key": apiKey,
+          "User-Agent": "EVChargingFinderApp/1.0 (Flutter)",
+        },
+      );
+
+      print("STATUS: ${response.statusCode}");
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        print("Stations found: ${data.length}");
+
+        Set<Marker> newMarkers = {};
+
+        for (var station in data) {
+          final info = station["AddressInfo"];
+
+          if (info != null &&
+              info["Latitude"] != null &&
+              info["Longitude"] != null) {
+
+            newMarkers.add(
+              Marker(
+                markerId: MarkerId(station["ID"].toString()),
+                position: LatLng(
+                  (info["Latitude"] as num).toDouble(),
+                  (info["Longitude"] as num).toDouble(),
+                ),
+                infoWindow: InfoWindow(
+                  title: info["Title"] ?? "Charging Station",
+                  snippet: info["AddressLine1"] ?? "",
+                ),
+              ),
+            );
+          }
+        }
+
+        setState(() {
+          _markers = newMarkers;
+          print("Markers added: ${_markers.length}");
+
+        });
+
+        if (_mapController != null && newMarkers.isNotEmpty) {
+          _mapController!.animateCamera(
+            CameraUpdate.newLatLngZoom(
+              newMarkers.first.position,
+              13,
+            ),
+          );
+        }
+      } else {
+        print(response.body);
+      }
+    } catch (e) {
+      print("ERROR: $e");
+    }
+  }
+
+  // Future<void> fetchStations(double lat, double lon) async {
+  //
+  //   final url = Uri.parse(
+  //       "https://api.openchargemap.io/v3/poi/"
+  //           "?output=json"
+  //           "&latitude=$lat"
+  //           "&longitude=$lon"
+  //           "&distance=5"
+  //           "&distanceunit=KM"
+  //           "&maxresults=20"
+  //   );
+  //
+  //   final response = await http.get(
+  //     url,
+  //     headers: {
+  //       "X-API-Key": apiKey,
+  //       "Content-Type": "application/json",
+  //     },
+  //   );
+  //
+  //   if (response.statusCode == 200) {
+  //     final data = jsonDecode(response.body);
+  //
+  //     Set<Marker> newMarkers = {};
+  //
+  //     for (var station in data) {
+  //       final info = station["AddressInfo"];
+  //       if (info != null &&
+  //           info["Latitude"] != null &&
+  //           info["Longitude"] != null) {
+  //
+  //         newMarkers.add(
+  //           Marker(
+  //             markerId: MarkerId(info["Title"] ?? "Station"),
+  //             position: LatLng(
+  //               info["Latitude"],
+  //               info["Longitude"],
+  //             ),
+  //             infoWindow: InfoWindow(
+  //               title: info["Title"] ?? "Charging Station",
+  //               snippet: info["AddressLine1"] ?? "No address",
+  //             ),
+  //             icon: BitmapDescriptor.defaultMarkerWithHue(
+  //               BitmapDescriptor.hueGreen,
+  //             ),
+  //           ),
+  //         );
+  //       }
+  //     }
+  //
+  //     setState(() {
+  //       _markers = newMarkers;
+  //     });
+  //     print("Stations count: ${data.length}");
+  //
+  //   }
+  // }
 
   Future<void> _getCurrentLocation() async {
     await Permission.location.request();
@@ -56,6 +191,9 @@ class _HomeScreenState extends State<HomeScreen> {
       _currentPosition =
           LatLng(position.latitude, position.longitude);
     });
+
+    // 🔥 CALL API HERE
+    await fetchStations(position.latitude, position.longitude);
   }
 
   @override
@@ -80,20 +218,23 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
             myLocationEnabled: true,
             myLocationButtonEnabled: true,
-            markers: {
-              Marker(
-                markerId: const MarkerId('station1'),
-                position: LatLng(
-                  _currentPosition!.latitude + 0.002,
-                  _currentPosition!.longitude + 0.002,
-                ),
-                icon: BitmapDescriptor.defaultMarkerWithHue(
-                    BitmapDescriptor.hueGreen),
-                infoWindow: const InfoWindow(
-                  title: "EV Charging Station",
-                ),
-              ),
-            },
+            markers: _markers,
+
+            // markers: {
+
+            //   Marker(
+            //     markerId: const MarkerId('station1'),
+            //     position: LatLng(
+            //       _currentPosition!.latitude + 0.002,
+            //       _currentPosition!.longitude + 0.002,
+            //     ),
+            //     icon: BitmapDescriptor.defaultMarkerWithHue(
+            //         BitmapDescriptor.hueGreen),
+            //     infoWindow: const InfoWindow(
+            //       title: "EV Charging Station",
+            //     ),
+            //   ),
+            // },
             onMapCreated: (controller) {
               _mapController = controller;
             },
